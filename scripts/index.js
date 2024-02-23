f9f01fe4969da1a94c5af550049b959091a03f22
Orlando Axalco <147562150+Oaxalco@users.noreply.github.com>
2024-02-22 20:25:12 -0800

    commit

--- scripts/index.js
+++ scripts/index.js
@@ -25,4 +25,35 @@ const initialCards = [
   },
 ];
 
-console.log(initialCards);
+const profileEditBtn = document.querySelector(".profile__edit-button");
+const profileEditModal = document.querySelector(".modal-edit");
+const profileEditModalCloseBtn =
+  profileEditModal.querySelector(".modal__close");
+const profileTitle = document.querySelector(".profile__title");
+const profileDescription = document.querySelector(".profile__description");
+const modalTitleInput = document.querySelector(".modal__form-title");
+const modalDescriptionInput = document.querySelector(
+  ".modal__form-description"
+);
+const profileEditForm = profileEditModal.querySelector(".modal__profile-form");
+const cardListElement = document.querySelector(".cards__list");
+const cardTemplate =
+  document.querySelector(".card__template").content.firstElementChild;
+const addNewCardModal = document.querySelector(".modal-add");
+const addNewCardBtn = document.querySelector(".profile__add-button");
+const addNewCardCloseBtn = addNewCardModal.querySelector(".modal__close");
+const addNewCardForm = addNewCardModal.querySelector(".modal__add-form");
+const cardTitleInput = addNewCardModal.querySelector(
+  ".modal__input_type_title"
+);
+const cardUrlInput = addNewCardModal.querySelector(".modal__input_type_url");
+const imageModal = document.querySelector(".modal-image");
+const imageModalCloseBtn = imageModal.querySelector(".modal__close");
+
+function closeModal(modal) {
+  modal.classList.remove("modal_opened");
+}
+
+function openModal(modal) {
+  modal.classList.add("modal_opened");
+}
